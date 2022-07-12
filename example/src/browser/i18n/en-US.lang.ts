e843c29fcfb5333e09c6a8ad9706b53ec8a30d5d
Dan <danwu.wdw@alibaba-inc.com>
2022-07-12 12:04:34 +0800

    chore: add i18n sample (#6)
    * chore: add i18n sample
    
    * chore: add new samples on README.md

---
+++ example/src/browser/i18n/en-US.lang.ts
@@ -0,0 +1,12 @@
+export const localizationBundle = {
+  languageId: 'en-US',
+  languageName: 'english',
+  localizedLanguageName: 'English',
+  contents: {
+    // 覆盖
+    'explorer.title': 'File',
+  
+    // 自定义
+    'common.ok': 'Confirm',
+  },
+};
---
+++ example/src/browser/i18n/zh-CN.lang.ts
@@ -0,0 +1,12 @@
+export const localizationBundle = {
+  languageId: 'zh-CN',
+  languageName: 'Chinese',
+  localizedLanguageName: '中文(中国)',
+  contents: {
+    // 覆盖
+    'explorer.title': '文件',
+
+    // 自定义
+    'common.ok': '确认',
+  },
+};
