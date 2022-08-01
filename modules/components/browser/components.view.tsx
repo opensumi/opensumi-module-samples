506a2aa0b3eda82585cc18a6a444ca8525488ac7
kuiwu <danwu.wdw@alibaba-inc.com>
2022-08-01 15:55:58 +0800

    chore: add select demo

--- modules/components/browser/components.view.tsx
+++ modules/components/browser/components.view.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Dropdown } from '@opensumi/ide-components/lib/dropdown';
-import { Button } from '@opensumi/ide-components'
+import { Button, Select, Option } from '@opensumi/ide-components'
 
 import * as styles from './components.module.less';
 
@@ -8,12 +8,26 @@ import * as styles from './components.module.less';
 const Drop = Dropdown as any;
 
 export const ComponentsSampleView = () => {
+  const [selected, setSelected] = React.useState<string>('lucy');
+  const handleChange = (value: string) => {
+    console.log(`selected ${value}`);
+    setSelected(value);
+  };
   return (
     <div className={styles.components_wrap}>
       <h1 className={styles.title}>Dropdown</h1>
       <Drop className={'kt-menu'} overlay={<div className={styles.menu_item}>I'm a Menu Item</div>} trigger={['click']}>
         <Button>Click Dropdown Menu</Button>
       </Drop>
+      <h1 className={styles.title}>Select</h1>
+      <Select value={selected} className={styles.select} onChange={handleChange}>
+        <Option value="jack">Jack</Option>
+        <Option value="lucy">Lucy</Option>
+        <Option value="disabled" disabled>
+          Disabled
+        </Option>
+        <Option value="Yiminghe">yiminghe</Option>
+    </Select>
     </div>
   );
 };
