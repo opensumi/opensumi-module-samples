42a6f0a836074cdca15b2febb9d0c9465e2c1e7d
kuiwu <danwu.wdw@alibaba-inc.com>
2023-02-28 15:45:24 +0800

    chore(deps): upgrade opensumi to v2.22.5

--- example/configs/webpack.ext-host.config.js
+++ example/configs/webpack.ext-host.config.js
@@ -47,7 +47,7 @@ module.exports = {
     },
   ],
   resolveLoader: {
-    modules: [path.join(__dirname, './node_modules')],
+    modules: [path.join(__dirname, '../node_modules')],
     extensions: ['.ts', '.tsx', '.js', '.json', '.less'],
     mainFields: ['loader', 'main'],
     moduleExtensions: ['-loader'],
